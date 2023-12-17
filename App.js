9f598e0d6fb04792daf2c322ff33616cf3778d15
Jessa Mae <143053099+resmajessa@users.noreply.github.com>
2023-12-18 00:09:03 +0800

    App.js

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ScrollView, TouchableOpacity } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { data } from './datas';
+import { Icon } from '@rneui/themed';
 
+
+  
 export default function App() {
+
+  const fullName = (data) => {
+    alert(data)
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Entypo/>
+        <FlatList
+          data={data}
+          keyExtractor={(index) => index.id.toString()}
+          renderItem={({item}) => (
+
+            <View style={styles.data}>
+              <Image
+                style={styles.image}
+                resizeMode='cover'
+                source={{uri: item.avatar}}
+              />
+                <View>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.address}>{item.address}</Text>
+                </View>
+
+                <View style={{flex:1}}>
+                  <TouchableOpacity 
+                    onPress={()=>fullName(`${'Name: '}${item.name}${'\n'}${'Nickname: '}${item.alias}${'\n'}${'Bounty: '}${item.bounty}${'\n'}${'Address: '}${item.address}`)}>
+                      <Icon
+                        name='dots-three-horizontal'
+                        type='entypo'
+                        size={25}
+                        style={styles.dot}
+                        />
+                  </TouchableOpacity>
+                </View>
+            </View>
+          )}/>
+      </ScrollView>
     </View>
   );
 }
@@ -15,6 +55,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    marginTop:20,
+  },
+  name: {
+    fontSize: 15,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  address: {
+    fontSize: 13,
+    marginBottom: 20,
+  },
+  dot: {
+    padding: 12,
+    flexDirection: 'row-reverse',
+    marginLeft: 16,
+  },
+  data: {
+    flexDirection: 'row',
+    marginBottom: 6,
+  },
+  image: {
+    marginRight: 20,
+    borderRadius: 400/2,
+    height: 50,
+    width: 50,
   },
 });
+
